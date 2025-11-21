a74b0beb7f8731463c7e57724758d0c63349db84
Kevin Warren <RhinoDBA@hotmail.com>
2025-11-21 16:54:10 -0500

    Added report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from stats import get_num_words
+from stats import get_num_words, get_char_counts, get_sorted_char_list
 
 def get_book_text(file_path):
     with open(file_path, 'r') as f:
@@ -9,7 +9,20 @@ def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
     num_words = get_num_words(text)
+    #print(f"Found {num_words} total words")
+    char_counts = get_char_counts(text)
+    #print(char_counts)
+    sorted_chars = get_sorted_char_list(char_counts)
+ 
+ # print the report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-
+    print("------- Character Count ---------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============ END ===============")
+    
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_counts(text):
+    text = text.lower()
+    char_counts = {}
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+
+def sort_on(item):
+    return item["num"]
+
+def get_sorted_char_list(char_counts):
+    sorted_list = []
+    for char, count in char_counts.items():
+        if char.isalpha():  # skip spaces, punctuation, newline, etc.
+            sorted_list.append({"char": char, "num": count})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
