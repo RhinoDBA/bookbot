ac8eb70f2d1fff4cd0d1333deda5f07f291b1a0d
Kevin Warren <RhinoDBA@hotmail.com>
2025-11-21 15:41:17 -0500

    Modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)
+    num_words = count_words(text)
+    print(f"found {num_words} total words")
+
+if __name__ == "__main__":
+    main()
