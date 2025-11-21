0071947f0ab1cc59c44698f5f0c9d8ca0a54b7f2
Kevin Warren <RhinoDBA@hotmail.com>
2025-11-21 16:01:40 -0500

    added stats.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+# main.py
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path, 'r') as f:
         return f.read()
-    
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
-    num_words = count_words(text)
-    print(f"found {num_words} total words")
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()
